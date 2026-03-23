1ade1309eff8e4a49d4195c361d924159d1d224b
Mathijs Van Dam <matv1991@gmail.com>
2026-03-23 14:13:47 -0400

    feat: refactor login negative tests to parameterised loop

--- saucedemo/tests/login.spec.ts
+++ saucedemo/tests/login.spec.ts
@@ -1,7 +1,16 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
-test('user can log in to Swag Labs', async ({ page }) => {
+const loginErrorScenarios = [
+  { description: 'invalid credentials', username: 'nonexistant user', password: 'test123', expectedError: 'Epic sadface: Username and password do not match any user in this service' },
+  { description: 'empty fields', username: '', password: '', expectedError: 'Epic sadface: Username is required' },
+  { description: 'username only', username: 'standard_user', password: '', expectedError: 'Epic sadface: Password is required' },
+  { description: 'password only', username: '', password: 'secret_sauce', expectedError: 'Epic sadface: Username is required' },
+];
+
+test('user can log in to Swag Labs', {
+  tag: ['@smoke', '@regression']
+}, async ({ page }) => {
   // GIVEN I am on the login page
   // WHEN I enter a valid username and password
   // AND I click the login button
@@ -15,54 +24,15 @@ test('user can log in to Swag Labs', async ({ page }) => {
   await expect(loginPage.pageTitle).toHaveText('Products');
 });
 
-test('user sees error with invalid login', async ({ page }) => {
-  // GIVEN I am on the login page
-  // WHEN I enter an invalid username and password
-  // AND I click the login button
-  // THEN an error message is displayed
-
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.login('wrong_user', 'wrong_password');
-  await loginPage.assertErrorMessage('Epic sadface: Username and password do not match any user in this service');
-
-});
-
-test('user sees error when attempting to login with empty fields', async ({ page }) => {
-  // GIVEN I am on the login page
-  // WHEN I click the login button without entering credentials
-  // THEN an error message is displayed
-
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.clickLogin();
-
-  await loginPage.assertErrorMessage('Epic sadface: Username is required');
-});
-
-test('user sees error when attempting to login providing username only', async ({ page }) => {
-  // GIVEN I am on the login page
-  // WHEN I enter a valid username only
-  // AND I click the login button
-  // THEN an error message is displayed
-
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.fillUsername('standard_user');
-  await loginPage.clickLogin();
-  await loginPage.assertErrorMessage('Epic sadface: Password is required');
-});
-
-test('user sees error when attempting to login providing password only', async ({ page }) => {
-  // GIVEN I am on the login page
-  // WHEN I enter a valid password only
-  // AND I click the login button
-  // THEN an error message is displayed
-
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.fillPassword('secret_sauce');
-  await loginPage.clickLogin();
-
-  await loginPage.assertErrorMessage('Epic sadface: Username is required');
-});
+for (const { description, username, password, expectedError } of loginErrorScenarios) {
+  test(`login error: ${description}`, {
+    tag: ['@regression'],
+  }, async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    if (username) await loginPage.fillUsername(username);
+    if (password) await loginPage.fillPassword(password);
+    await loginPage.clickLogin();
+    await loginPage.assertErrorMessage(expectedError);
+  });
+}
